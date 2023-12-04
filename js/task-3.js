f2f73d8e35b347cc880cb3897be1a47906b944aa
Costbound <popovskiy96@gmail.com>
2023-12-04 11:55:44 +0100

    final

---
+++ js/task-3.js
@@ -0,0 +1,3 @@
+function checkForSpam(message) {
+    return message.toLowerCase().includes("spam") || message.toLowerCase().includes("sale");
+}
--- task-3.js
+++
@@ -1,9 +0,0 @@
-function checkForSpam(message) {
-    if (message.toLowerCase().includes("spam")) {
-        return true;
-    } else if (message.toLowerCase().includes("sale")) {
-        return true;
-    }else {
-        return false;
-    }
-}
